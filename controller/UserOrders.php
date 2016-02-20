988a6e0041ffe377ed6a6bdb5ab1fa4c51dd90ad
korabieka <hussienashour@ymail.com>
2016-02-20 04:54:54 -0500

    jquery of userOrders

---
+++ controller/UserOrders.php
@@ -0,0 +1,8 @@
+<?php
+require_once("../include/Validation.php"); // deconnection already included in Validation
+session_start();
+$notes = $_POST['notes'];
+$room = $_POST['rooms'];
+$total = $_POST['total'];
+
+?>
